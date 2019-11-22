5e13d1a2d56c7c0a92844401f16e4ec3c78086df
Alexander Letnikow <reclamme@gmail.com>
2019-11-22 08:47:03 +0200

    Implement set YII_ENV and YII_DEBUG from environment variables

---
+++ src/constants.php
@@ -0,0 +1,4 @@
+<?php declare(strict_types=1); // @codeCoverageIgnoreStart
+
+defined("YII_ENV") || define("YII_ENV", getenv("YII_ENV") ?: "dev");
+defined("YII_DEBUG") || define("YII_DEBUG", (bool)getenv("YII_DEBUG") ?: false);
