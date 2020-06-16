c4582c9df168929aa2f59107de4215a104881951
horat1us <reclamme@gmail.com>
2020-06-16 17:45:41 +0300

    Fix tests

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -2,46 +2,27 @@
 
 namespace Wearesho\Yii2\DotEnv;
 
-use Dotenv\Exception\InvalidPathException;
 use Dotenv\Dotenv;
 use yii\base;
 
-defined('YII_ENV') or define('YII_ENV', 'prod');
-
 class Bootstrap extends base\BaseObject implements base\BootstrapInterface
 {
-    public ?string $file = '.env.' . YII_ENV;
+    public array $paths = [];
 
-    public ?string $overrideFile = '.env';
+    public array $names = [];
 
     public function bootstrap($app): array
     {
-        $paths = $this->getPaths($app);
-        if (!is_null($this->file)) {
-            $dotEnv = Dotenv::create($paths, $this->file);
-            $environment = $dotEnv->load();
-        } else {
-            $environment = [];
-        }
-
-        if (!is_null($this->overrideFile)) {
-            $dotEnv = Dotenv::create($paths, $this->overrideFile);
-            try {
-                $environment = array_merge($environment, $dotEnv->overload());
-            } catch (InvalidPathException $exception) {
-                // supress missing optional override file not found exception
-            }
-        }
-
-        return $environment;
-    }
-
-    protected function getPaths(base\Application $app): array
-    {
-        $basePath = $app->getBasePath();
-        return [
-            $basePath, // support yii2-app-basic
-            dirname($basePath), // support yii2-app-advanced,
+        $paths = $this->paths ? $this->paths : [
+            $app->getBasePath(),
+            dirname($app->getBasePath()),
         ];
+        $names = $this->names ? $this->names : [
+            '.env',
+            '.env.' . YII_ENV,
+        ];
+        $dotEnv = Dotenv::createUnsafeMutable($paths, $names);
+        \Yii::$container->setSingleton(Dotenv::class, $dotEnv);
+        return $dotEnv->safeLoad();
     }
 }
--- tests/BootstrapTest.php
+++ tests/BootstrapTest.php
@@ -2,44 +2,37 @@
 
 namespace Wearesho\Yii2\DotEnv\Tests;
 
-use Dotenv\Exception\InvalidPathException;
 use PHPUnit\Framework\TestCase;
 use Wearesho\Yii2\DotEnv;
 use yii\base;
 
 class BootstrapTest extends TestCase
 {
-    public function provider(): array
+    public function dataProvider(): array
     {
         return [
-            [null, null, [],],
-            ['.env.prod', null, ['DOTENV' => 'prod',],],
-            ['.env.missing', null, InvalidPathException::class,],
-            ['.env.prod', '.env.missing', ['DOTENV' => 'prod',],],
-            ['.env.prod', '.env', ['DOTENV' => 'override',],],
-            [null, '.env', ['DOTENV' => 'override',],],
-            [null, '.env.missing', [],],
+            [[], ['DOTENV' => 'dev',],],
+            [['names' => ['.env.test',],], ['DOTENV' => 'test',],],
+            [['names' => ['.env.custom'],], ['DOTENV' => 'override',],],
+            [['names' => ['.env.2']], []]
         ];
     }
 
     /**
-     * @dataProvider provider
+     * @dataProvider dataProvider
      */
-    public function testBootstrap(?string $file, ?string $overrideFile, $expected): void
+    public function testBootstrap(array $config, array $expected): void
     {
         $app = $this->mockApplication();
-        $bootstrap = new DotEnv\Bootstrap(compact('file', 'overrideFile'));
+        $bootstrap = new DotEnv\Bootstrap($config);
 
-        if (is_string($expected) && class_exists($expected)) {
-            $this->expectException($expected);
-        }
         $environment = $bootstrap->bootstrap($app);
         $this->assertEquals($expected, $environment);
     }
 
     protected function mockApplication(): base\Application
     {
-        $mock = $this->createMock(base\Application::class);
+        $mock = $this->createMock(base\Application::class, ['getBasePath']);
         $mock->method('getBasePath')->willReturn(__DIR__ . DIRECTORY_SEPARATOR . 'data');
         return $mock;
     }
